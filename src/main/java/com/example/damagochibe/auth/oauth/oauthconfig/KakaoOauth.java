de7c3a76567f9684195e9a4e272f366b2f0266d9
kkssii0210 <kkssii0210@naver.com>
2024-01-12 09:46:20 +0900

    위험요소 주석

--- src/main/java/com/example/damagochibe/auth/oauth/oauthconfig/KakaoOauth.java
+++ src/main/java/com/example/damagochibe/auth/oauth/oauthconfig/KakaoOauth.java
@@ -3,7 +3,7 @@ package com.example.damagochibe.auth.oauth.oauthconfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
-public class KakaoOauth implements SocialOauth{
-}
+//@Component
+//@RequiredArgsConstructor
+//public class KakaoOauth implements SocialOauth{
+//}
--- src/main/java/com/example/damagochibe/auth/oauth/oauthconfig/SocialOauth.java
+++ src/main/java/com/example/damagochibe/auth/oauth/oauthconfig/SocialOauth.java
@@ -3,10 +3,10 @@ package com.example.damagochibe.auth.oauth.oauthconfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 
-public interface SocialOauth {
-    String getOauthRedirectURL();
-    ResponseEntity<String> requestAccessToken(String code);
-    SocialOauthToken getAccessToken(ResponseEntity<String> response) throws JsonProcessingException;
-    ResponseEntity<String> requestUserInfo(SocialOauthToken oauthToken);
-    SocialUser getUserInfo(ResponseEntity<String> userInfoRes) throws JsonProcessingException;
-}
+//public interface SocialOauth {
+//    String getOauthRedirectURL();
+//    ResponseEntity<String> requestAccessToken(String code);
+//    SocialOauthToken getAccessToken(ResponseEntity<String> response) throws JsonProcessingException;
+//    ResponseEntity<String> requestUserInfo(SocialOauthToken oauthToken);
+//    SocialUser getUserInfo(ResponseEntity<String> userInfoRes) throws JsonProcessingException;
+//}
