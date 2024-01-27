05a2de50d2cda3c3e60c6d13d87fec1e2fbff87d
jeon602 <731aa@naver.com>
2024-01-27 13:06:29 +0900

    comment CRUD

--- src/main/java/com/example/damagochibe/comment/entity/Comment.java
+++ src/main/java/com/example/damagochibe/comment/entity/Comment.java
@@ -1,4 +1,23 @@
 package com.example.damagochibe.comment.entity;
 
+import com.example.damagochibe.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(schema = "damagochi")
+@NoArgsConstructor
+@Getter @Setter
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="comment_id")
+    private Long id;
+    private String content;
+    @ManyToOne(cascade = CascadeType.PERSIST,fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private
+
 }
