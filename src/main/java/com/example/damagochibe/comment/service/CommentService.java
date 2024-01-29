3f53a103256382502f2615df1078bfeecebb3a3c
jeon602 <731aa@naver.com>
2024-01-29 11:09:45 +0900

    comment &  post  CRUD 추가 중..

--- src/main/java/com/example/damagochibe/comment/service/CommentService.java
+++ src/main/java/com/example/damagochibe/comment/service/CommentService.java
@@ -1,4 +1,21 @@
 package com.example.damagochibe.comment.service;
 
+import com.example.damagochibe.comment.entity.Comment;
+import com.example.damagochibe.comment.repository.CommentRepository;
+import com.example.damagochibe.member.entity.Member;
+import com.example.damagochibe.post.entity.Post;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class CommentService {
+    //private final CommentRepository commentRepository;
+    //create comment
+
+    //change comment
+
 }
--- src/main/java/com/example/damagochibe/post/entity/Post.java
+++ src/main/java/com/example/damagochibe/post/entity/Post.java
@@ -1,39 +1,42 @@
 package com.example.damagochibe.post.entity;
 
 import com.example.damagochibe.comment.entity.Comment;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.example.damagochibe.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.parameters.P;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(schema = "damagochi")
 @NoArgsConstructor
-@Getter @Setter
+@Getter
+@Setter
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="post_id")
+    @Column(name = "post_id")
     private Long id;
+
+    @Column(length = 1000, nullable = false)
     private String title;
 
-    @Column(name="content", length = 3000)
+    @Column(columnDefinition = "VARCHAR(500)", nullable = false)
     private String content;
 
-    @OneToMany(mappedBy = "post")
-    @JsonManagedReference
-    private List<Comment> comments = new ArrayList<>();
 
-
-    public Post(Long id, String title, String content, List<Comment> comments){
-        this.id = id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @OneToMany(mappedBy = "post", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @OrderBy("createdDate asc ") // 댓글 정렬
+    private List<Comment> comments;
+    /* 게시글 수정 메소드 */
+    public void update (String title, String content) {
         this.title = title;
         this.content = content;
-        this.comments = comments;
     }
 }
+
